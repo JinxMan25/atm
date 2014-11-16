57d3acd2aa8f1d8e91b5891d821462ab5227aa26
Md Samiul Huq <samiulg3@gmail.com>
2014-11-16 18:47:25 -0500

    photo upload attrs

--- models/Photos.js
+++ models/Photos.js
@@ -1,8 +1,25 @@
 var mongoose = require('mongoose')
+var thumbnailPluginLib = require('mongoose-thumnail')
+var thumbnailPlugin = thumbnailPluginLib.thumnailPlugin;
+var make_upload_to_model = thumbnailPluginLib.make_upload_to_model;
+
+var uploads_base = path.join(__dirname, "uploads");
+var uploads = path.join(uploads_base, "u");
 
 var PhotoSchema = new mongoose.Schema({
   title: String,
   posted: Date.now,
   link: String,
   upvotes: {type: Number, default: 0});
+
+PhotoSchema.plugin(thumnailPlugin, {
+  name: "photo",
+  format: "png",
+  size: "80",
+  inline: false,
+  save: true,
+  upload_to: make_upload_to_model(uploads, 'photos'),
+      relative_to: uploads_base
+});
+var SampleModel = db.model("Photo", SampleSchema);
 
