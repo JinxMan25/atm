c357543e842086e96f42f12f3f43d4167dc79435
Md Samiul Huq <samiulg3@gmail.com>
2014-11-18 20:48:21 -0500

    get all photos

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Photo = mongoose.model('Photo');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {
+  Photo.find(function(err, photos){
+    if (err) { return next(err); }
+
+    res.json(photos);
+  });
 });
 
 module.exports = router;
