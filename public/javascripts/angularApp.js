582929ef3813ca2a9116be6788e6c65ec6572e4e
Md Samiul Huq <samiulg3@gmail.com>
2014-11-29 23:35:46 -0500

    demo

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,5 +1,11 @@
 var app = angular.module('atthismoment', [])
 
 app.controller('mainController', ['$scope', function($scope){
-  $scope.test = "Hellow World";
+  $scope.posts = [
+  {title: 'post 1', upvotes: 5},
+  {title: 'post 2', upvotes: 2},
+  {title: 'post 3', upvotes: 15},
+  {title: 'post 4', upvotes: 9},
+  {title: 'post 5', upvotes: 4}
+  ];
 }]);
