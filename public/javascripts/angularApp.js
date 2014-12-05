a1956fc04a4ef138927a33dd9623530e9799a8f6
Md Samiul Huq <samiulg3@gmail.com>
2014-12-04 22:38:49 -0500

    add a photo angular service

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -2,5 +2,15 @@ angular.module('atm', [])
 .controller('ctrl', [
 '$scope',
 function($scope){
-  $scope.test = 'Hello world!';
+  $scope.photos = [
+{ title: "hi", upvotes: 12 },
+{ title: "hisdf", upvotes: 1 },
+{ title: "hiss", upvotes: 2 },
+{ title: "hi", upvotes: 3 },
+{ title: "hi", upvotes: 4 },
+];
+
+  $scope.addPhoto = function(){
+    $scope.photos.push({title: $scope.title, upvotes: 0});
+  };
 }]);
