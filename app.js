8a13bfde661704fea2f82d83f1eb9efc0999b835
Md Samiul Huq <samiulg3@gmail.com>
2014-12-04 21:36:00 -0500

    angular test working

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
+app.locals.delimiters = '<% %>';
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,11 +1,6 @@
-var app = angular.module('atthismoment', [])
-
-app.controller('mainController', ['$scope', function($scope){
-  $scope.posts = [
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 15},
-  {title: 'post 4', upvotes: 9},
-  {title: 'post 5', upvotes: 4}
-  ];
+angular.module('atm', [])
+.controller('ctrl', [
+'$scope',
+function($scope){
+  $scope.test = 'Hello world!';
 }]);
