a563597365d6baf0902e60777bf6f3525f0764fe
Md Samiul Huq <samiulg3@gmail.com>
2014-12-06 22:10:34 -0500

    directive and controller for slider

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -16,3 +16,25 @@ function($scope){
     $scope.title = '';
   };
 }]);
+
+atm.directive('slider', function($timeout){
+  return {
+    restrict: 'AE',
+    replace: true,
+    scope: {
+      images: '='
+    },
+    link: function(scope, elem, attrs){},
+    templateUrl: '/home/sami/atm/views/template.html'
+  };
+});
+
+atm.controller('SliderController', function($scope){
+  $scope.images = [
+    { src: 'image1.png', title: 'Pic 1' },
+    { src: 'image2.png', title: 'Pic 2' },
+    { src: 'image3.png', title: 'Pic 3' },
+    { src: 'image4.png', title: 'Pic 4' },
+    { src: 'image5.png', title: 'Pic 5' },
+  ];
+});
