65dd0ff3969d3cddafdfeaaa8d6b3725a9f5afa6
Md Samiul Huq <samiulg3@gmail.com>
2014-12-07 01:06:24 -0500

    angular image slider

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,4 +1,4 @@
-var atm = angular.module('atm', []);
+var atm = angular.module('atm', ['ngAnimate']);
 
 atm.controller('ctrl', [
 '$scope',
@@ -17,24 +17,62 @@ function($scope){
   };
 }]);
 
-atm.directive('slider', function($timeout){
+atm.directive('slider', function ($timeout) {
   return {
     restrict: 'AE',
-    replace: true,
-    scope: {
-      images: '='
+  replace: true,
+  scope:{
+    images: '='
+  },
+    link: function (scope, elem, attrs) {
+  
+    scope.currentIndex=0;
+
+    scope.next=function(){
+      scope.currentIndex<scope.images.length-1?scope.currentIndex++:scope.currentIndex=0;
+    };
+    
+    scope.prev=function(){
+      scope.currentIndex>0?scope.currentIndex--:scope.currentIndex=scope.images.length-1;
+    };
+    
+    scope.$watch('currentIndex',function(){
+      scope.images.forEach(function(image){
+        image.visible=false;
+      });
+      scope.images[scope.currentIndex].visible=true;
+    });
+    
+    /* Start: For Automatic slideshow*/
+    
+    var timer;
+    
+    var sliderFunc=function(){
+      timer=$timeout(function(){
+        scope.next();
+        timer=$timeout(sliderFunc,500);
+      },3000);
+    };
+    
+    sliderFunc();
+    
+    scope.$on('$destroy',function(){
+      $timeout.cancel(timer);
+    });
+    
+    /* End : For Automatic slideshow*/
+    
     },
-    link: function(scope, elem, attrs){},
-    templateUrl: '/home/sami/atm/views/template.html'
-  };
+  templateUrl:'templateUrl.html'
+  }
 });
 
 atm.controller('SliderController', function($scope){
   $scope.images = [
-    { src: 'image1.png', title: 'Pic 1' },
-    { src: 'image2.png', title: 'Pic 2' },
-    { src: 'image3.png', title: 'Pic 3' },
-    { src: 'image4.png', title: 'Pic 4' },
-    { src: 'image5.png', title: 'Pic 5' },
+    { src: 'image1.jpg', title: 'Pic 1', url: "http://samiulhuq.com" },
+    { src: 'image2.jpg', title: 'Pic 2', url: "http://samiulhuq.com" },
+    { src: 'image3.jpg', title: 'Pic 3', url: "http://samiulhuq.com" },
+    { src: 'image4.jpg', title: 'Pic 4', url: "http://samiulhuq.com" },
+    { src: 'image5.jpg', title: 'Pic 5', url: "http://samiulhuq.com" },
   ];
 });
