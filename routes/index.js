ef2819b42413ed5c8287dbe68620e0ebad6a1df2
MD Huq <samiulg3@gmail.com>
2015-07-14 18:40:27 +0300

    bugfix: increment vote leads to homepage

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var formidable = require('formidable');
 var levenshtein = require('levenshtein');
 
 /* GET home page. */
-router.get('/atm/get', function(req, res, next) {
+router.get('/atm/get', function(req, res, next){
 
   var limit = req.query.limit || 10;
   var maxDistance = req.query.distance || 100;
@@ -128,10 +128,10 @@ router.put('/get/:uniq_token/downvote', function(req,res, next){
 
 function randomValueBase64 (len) {
     return crypto.randomBytes(Math.ceil(len * 3 / 4))
-        .toString('base64')   // convert to base64 format
-        .slice(0, len)        // return required number of characters
-        .replace(/\+/g, '0')  // replace '+' with '0'
-        .replace(/\//g, '0'); // replace '/' with '0'
+      .toString('base64')   // convert to base64 format
+      .slice(0, len)        // return required number of characters
+      .replace(/\+/g, '0')  // replace '+' with '0'
+      .replace(/\//g, '0'); // replace '/' with '0'
 }
 
 module.exports = router;
